916d6b84f1493cf0a0225951d292997a4fe5d598
Chase Durbin <chase.durbin.02@gmail.com>
2023-04-15 12:59:24 -0500

    Updated game

--- Player.gd
+++ Player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 8.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 5.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -26,8 +26,8 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Handle Jump.	
+	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
